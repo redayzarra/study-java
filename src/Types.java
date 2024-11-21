6e2d31b82de1e45d51166a6889003b7a24b4216c
ReDay Zarra <redayzarra@MacBook.local>
2024-11-21 15:11:04 -0500

    Adding lesson on types

---
+++ src/Types.java
@@ -0,0 +1,20 @@
+
+import java.awt.Point;
+
+public class Types {
+	public static void main(String[] args) {
+		// Primitive types don't point to each other
+		int x = 1;
+		int y = x;
+		x = 2;
+		// The variables are independent
+		System.out.println(y + ", " + x);
+
+		// Reference types point to same object
+		Point point1 = new Point(1, 1);
+		Point point2 = point1;
+		point1.x = 5;
+		// The variables are dependent
+		System.out.println(point2);
+	}
+}
