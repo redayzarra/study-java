fa995882059967757d92c4e4f13eee865909ebc6
ReDay Zarra <redayzarra@gmail.com>
2024-11-20 23:44:04 -0500

    Rewriting variables doesn't require extra syntax

--- src/App.java
+++ src/App.java
@@ -1,9 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        int age = 30;
-        int temperature = 20;
-        age = 35;
-        System.out.println(age);
-        System.out.println(temperature);
+        // Initialize my age and then rewrite her age
+        int myAge = 22;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
