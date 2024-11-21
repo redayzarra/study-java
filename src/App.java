bb830c7b2721cb291cc2705369c09d32d09fadf2
ReDay Zarra <redayzarra@gmail.com>
2024-11-20 23:49:04 -0500

    Understanding primitive types in Java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        // Initialize my age and then rewrite her age
-        int myAge = 22;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 30; // Initialize my age as bytes
+        long viewsCount = 123_456_789L; // Use underscore and L for readability
+        float price = 10.99F; // Float to tell Java what it is
+
+        char letter = 'A'; // Single chars with single quotes
+
+        boolean isElgible = false; // Booleans are lowercase
     }
 }
