e4e763674473a5098a9bac95f8ed726d33951010
ReDay Zarra <redayzarra@MacBook.local>
2024-11-21 02:39:54 -0500

    Have to allocate memory for non-primitive types

--- src/App.java
+++ src/App.java
@@ -1,11 +1,10 @@
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) {
-        byte age = 30; // Initialize my age as bytes
-        long viewsCount = 123_456_789L; // Use underscore and L for readability
-        float price = 10.99F; // Float to tell Java what it is
-
-        char letter = 'A'; // Single chars with single quotes
-
-        boolean isElgible = false; // Booleans are lowercase
+        byte age = 30;
+        // Need to allocate memory for this type
+        Date now = new Date();
+        System.out.println(now);
     }
 }
