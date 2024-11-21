fbf78064aec935abbe0211a0d35cee04eaca0c3b
redayzarra <113388793+redayzarra@users.noreply.github.com>
2024-11-21 15:07:18 -0500

    Getting familiar with strings

--- src/App.java
+++ src/App.java
@@ -1,21 +1,8 @@
 
-import java.awt.Point;
 
 
 public class App {
     public static void main(String[] args) {
-        // Primitive types don't point to each other
-        int x = 1;
-        int y = x;
-        x = 2;
-        // The variables are independent
-        System.out.println(y);
-
-        // Reference types point to same object
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 5;
-        // The variables are dependent
-        System.out.println(point2);
+        
     }
 }
