f608bf6c0a219d862797b0ce7a7f8bbc661838bc
ReDay Zarra <redayzarra@MacBook.local>
2024-11-21 14:48:34 -0500

    Understanding the difference between reference and primitive types

--- src/App.java
+++ src/App.java
@@ -1,10 +1,21 @@
-import java.util.Date;
+
+import java.awt.Point;
+
 
 public class App {
     public static void main(String[] args) {
-        byte age = 30;
-        // Need to allocate memory for this type
-        Date now = new Date();
-        System.out.println(now);
+        // Primitive types don't point to each other
+        int x = 1;
+        int y = x;
+        x = 2;
+        // The variables are independent
+        System.out.println(y);
+
+        // Reference types point to same object
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 5;
+        // The variables are dependent
+        System.out.println(point2);
     }
 }
