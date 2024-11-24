7c0a404dfba8cd41b564422f4d034cc498881173
ReDay Zarra <redayzarra@gmail.com>
2024-11-24 00:01:52 -0500

    Adding compound assignment operators

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -3,6 +3,9 @@ public class Arithmetic {
 		// Similar to Python but with greater type safety
 		int res = 10 + 3;
 		res -= 13;
+		res += 5;
+		res *= 6;
+		res /= 3;
 		System.out.println(res);
 
 		// Since num is an integer => it will not have decimals
@@ -16,8 +19,11 @@ public class Arithmetic {
 		// We are copying the value of x to y then incrementing x
 		int x = 1;
 		int y = x++;
-		System.out.println(y + ", " + x); // Y doesn't increment with X
+		System.out.println(x + ", " + y); // Y doesn't increment with X
 
-		
+		// Now we are incrementing x then copying the value to y
+		x = 1;
+		y = ++x;
+		System.out.println(x + ", " + y); // Y also increments with X
 	}
 }
