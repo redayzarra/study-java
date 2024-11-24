716f39a6d65d05a8c1908f4ffad1f8b3c9b8aada
ReDay Zarra <redayzarra@gmail.com>
2024-11-23 23:50:31 -0500

    Understanding how to make immutable variables for constants

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+	public static void main(String[] args) {
+		// We can use "final" keyword to make immutable variables
+		final float PI = 3.14F;
+		System.out.println(PI);
+	}
+}
