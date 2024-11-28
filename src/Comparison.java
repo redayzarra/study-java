24e0fcfe96e52ec27136e0e4d1447924ffbdff32
ReDay Zarra <redayzarra@gmail.com>
2024-11-27 20:16:20 -0500

    Comparison operators are the same as Python

---
+++ src/Comparison.java
@@ -0,0 +1,12 @@
+public class Comparison {
+	public static void main(String[] args) {
+		// Create x and y to be the same values
+		int x = 1;
+		int y = 1;
+
+		// Similar to Python, check if equal: boolean expression
+		System.out.println(x == y);
+		System.out.println(x != y);
+		System.out.println(x <= y);
+	}
+}
