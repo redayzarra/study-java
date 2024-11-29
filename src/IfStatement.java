c56c444e3bea2d8aa4a3a620530d98c07b569a65
ReDay Zarra <redayzarra@gmail.com>
2024-11-28 22:56:41 -0500

    Starting with if-statements in java

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,3 +1,5 @@
 public class IfStatement {
-	
+	public static void main(String[] args) {
+		// Type here
+	}
 }
--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,3 +1,5 @@
 public class WhileLoop {
-	
+	public static void main(String[] args) {
+		// Type here
+	}
 }
