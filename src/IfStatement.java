2f30badb4a442910102442e403232e5572a1060f
ReDay Zarra <redayzarra@gmail.com>
2024-11-29 22:59:22 -0500

    Learning ternary operators and moving to switch statements

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -10,5 +10,12 @@ public class IfStatement {
 		} else {
 			System.out.println("It's a cold day!");
 		}
+
+		// Learning to write one-liner simplified if-statements
+		int income = 120_000;
+		boolean hasHighIncome = (income > 100_000);
+
+		// Learning the ternary operator
+		String className = (income > 100_000) ? "First" : "Economy";
 	}
 }
