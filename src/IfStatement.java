5241f41a92a0db7fac501e1e209e5ee04a1ff300
ReDay Zarra <redayzarra@gmail.com>
2024-11-29 22:51:05 -0500

    Learning to write if-statements in java

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,5 +1,14 @@
 public class IfStatement {
 	public static void main(String[] args) {
-		// Type here
+		// You can create a if-statement with (condition) {action}
+		int temp = 32;
+		if (temp > 30) {
+			System.out.println("It's a hot day!");
+			System.out.println("Drink water");
+		} else if (temp > 20 && temp <= 30) {
+			System.out.println("Beautiful day!");
+		} else {
+			System.out.println("It's a cold day!");
+		}
 	}
 }
