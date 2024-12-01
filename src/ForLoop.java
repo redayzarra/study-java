0ef07d4de936af1b3a3a469095c4a24866e12c0f
ReDay Zarra <redayzarra@gmail.com>
2024-12-01 11:11:25 -0500

    Going in reverse using for loops

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -4,5 +4,10 @@ public class ForLoop {
 		for (int index = 0; index < 5; index++) {
 			System.out.println("Hello world");
 		}
+
+		// Going in reverse
+		for (int index = 5; index > 0; index--) {
+			System.out.println("Bye world: " + index);
+		}
 	}
 }
