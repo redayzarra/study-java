670d66f5dac912d890cbe537790ad4c2d67733e8
ReDay Zarra <redayzarra@gmail.com>
2024-12-01 11:08:46 -0500

    Learning for loops in Java

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,5 +1,8 @@
 public class ForLoop {
 	public static void main(String[] args) {
-		// Type here
+		// We initialize index, the condition, and increment
+		for (int index = 0; index < 5; index++) {
+			System.out.println("Hello world");
+		}
 	}
 }
