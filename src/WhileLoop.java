f5aa12b0918c9c20a96fff456b5782735c21fd41
ReDay Zarra <redayzarra@gmail.com>
2024-12-01 12:32:26 -0500

    Learning break and continue in Java

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -16,14 +16,27 @@ public class WhileLoop {
 			// Ask for the input, and return it
 			System.out.print("Input: ");
 			input = scanner.next().toLowerCase();
-			System.out.println(input);
+			// Manually checking if we can echo
+			if (!input.equals("quit"))
+				System.out.println(input);
 		}
 
 		// Learning a do-while loop
 		do { 
 			System.out.print("Password: ");
 			input = scanner.next().toLowerCase();
+			// Using break instead of manual if-statement
+			if (input.equals("quit")) {
+				break;
+			}
+
+			// Using continue to skip current iteration
+			if (input.equals("pass")) {
+				continue;
+			}
+
+			// Echo the input back to the terminal
 			System.out.println(input);
-		} while (!input.equals("quit"));
+		} while (true);
 	}
 }
