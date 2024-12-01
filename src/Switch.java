4fbceedb437dcbbdd93d2827a80d338da2d09502
ReDay Zarra <redayzarra@gmail.com>
2024-11-30 20:33:01 -0500

    Learning basic switch statements format

--- src/Switch.java
+++ src/Switch.java
@@ -2,13 +2,17 @@ public class Switch {
 	public static void main(String[] args) {
 		// Define the variable that we are dependent on
 		String role = "admin";
+
+		// Create a switch block with the variable 
 		switch (role) {
+			// Define an individual case for the variable
 			case "admin":
 				System.out.println("You're an admin.");
-				break;
+				break; // Remember to break out of switch block
 			case "moderator":
 				System.out.println("You're a mod.");
 				break;
+			// Define a default case if all else fails
 			default:
 				System.out.println("You're a guest.");
 		}
