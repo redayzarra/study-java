5aec71acf38d9631dac1848e811457c32b71825f
ReDay Zarra <redayzarra@gmail.com>
2024-12-01 11:43:45 -0500

    Learning while loops in java

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class WhileLoop {
 	public static void main(String[] args) {
-		// Type here
+		int index = 5;
+		while (index > 0) {
+			System.out.println("Hello world: " + index);
+			index--;
+		}
+
+		String input = "";
+		Scanner scanner = new Scanner(System.in);
+		while (!input.equals("quit")) {
+			System.out.print("Input: ");
+			input = scanner.next().toLowerCase();
+			System.out.println(input);
+		}
 	}
 }
