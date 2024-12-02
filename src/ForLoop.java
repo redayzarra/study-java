efc8cf0bb0d809cb96aefbca8f6b05f164d09c19
ReDay Zarra <redayzarra@gmail.com>
2024-12-01 19:10:36 -0500

    Learning for-each loop to iterate over items

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -9,5 +9,17 @@ public class ForLoop {
 		for (int index = 5; index > 0; index--) {
 			System.out.println("Bye world: " + index);
 		}
+
+		// Using a manual for-loop to iterate over items in an array
+		String[] fruits = {"Apple", "Mango", "Orange"};
+		for (int index = 0; index < fruits.length; index++) {
+			System.out.println(fruits[index]);
+		}
+
+		// Using a for-each loop to iterate over items
+		for (String fruit: fruits) {
+			// We cannot iterate in reverse
+			System.out.println(fruit);
+		}
 	}
 }
